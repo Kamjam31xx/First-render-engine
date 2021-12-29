17f3e617317c6ef7fad8cd4059febf1ebdf5c2a3
Kamjam21xx <45414914+Kamjam21xx@users.noreply.github.com>
2021-12-29 16:41:54 -0500

    progress

--- src/OpenGlCourseApp/Maps/default_map_generator.py
+++ src/OpenGlCourseApp/Maps/default_map_generator.py
@@ -7,20 +7,53 @@ name = ""
 type = ""
 scale = 1
 default_chunk_id = 0
+width = 512
+height = 512
 
 #	starting on the top-left corner, generate an (n by n) map of connected nodes
 TAB_SIZE = 4
 G_TAB = 0
-NEIGHBOR_ID_INDEX = 0
-NEIGHBOR_X_INDEX = 1
-NEIGHBOR_Y_INDEX = 1
+
+class Neighbor :
+
+	def __init__(_id, _x, _y) :
+		
+		self.id = _id
+		self.x = _x
+		self.y = _y
+
+class Entity :
+
+	def __init__(_name, _x, _y, _z, _components) :
+
+		self.name = _name
+		self.x = _x
+		self.y = _y
+		self.z = _z
+		self.components = _components
+
+class GroundTile :
+
+	def __init___(_vertices, _indices) :
+		
+		self.vertices = _vertices
+		self.indices = _indices
+
+class Chunk2D :
+
+	def __init__(_id):
+
+		self.id = _id
+		self.neighbors = [];
+		self.entities = [];
+		self.ground_tiles = [];
 
 def tab( _n) :
 
 	count = (_n + G_TAB) * TAB_SIZE
 	string = ""
-	for i in range(_number) :
-		spaces += " "
+	for i in range(count) :
+		string += " "
 
 	return string
 
@@ -33,33 +66,71 @@ def qouteID(_id) :
 	
 	return "\"cid_" + _id + "\""
 
-def neighborString(_neighbor) :
+def neighborToString(_neighbor) :
 
-	_nid = _neighbor[NEIGHBOR_ID_INDEX]
-	_nx = _neighbor[NEIGHBOR_X_INDEX]
-	_ny = _neighbor[NEIGHBOR_Y_INDEX]
+	return "{ " + qoute("id") + " : " + _neighbor.id + ", " + qoute("x") + " : " + _neighbor.x + ", " + qoute("y") + " : " + _neighbor.y + " }"
 
-	return "{ " + qoute("id") + " : " + _nid + ", " + qoute("x") + " : " + _nx + ", " + qoute("y") + " : " + _ny + " } \n"
-
-def getChunkString(_id, _name, _neighbors, _entities, _ground_tile) :
+def getChunkString(_chunk) :
  
-	l = 0;
+	l = 0
 
-	chunk = tab(l) + qouteID(_id) + " : {\n"
+	chunk_str = tab(l) + qouteID(_chunk.id) + " :\n{\n"
+
 	l += 1
 
-	chunk += tab(l) + "\"name\" : " + name + ",\n"
+	chunk_str += tab(l) + "\"name\" : " + _chunk.name + ",\n"
 	
-	chunk += tab(l) + "\"neighbors\" : \n"
+	chunk_str += tab(l) + "\"neighbors\" :\n"
+
 	l += 1
 
-	chunk += tab(l) + "[\n"
-	for neighbor in _neighbors :
-		chunk += tab(l) + neighborString(neighbor)
-	chunk += tab(l) + "],\n"
+	chunk_str += tab(l) + "[\n"
+
+	l += 1
+
+	for neighbor in _chunk.neighbors :
+		chunk_str += tab(l) + neighborToString(neighbor) + "\n"
+
 	l -= 1
 
-	chunk += tab(l) + qoute("entities") + " : [],\n"
-	chunk += tab(l) + qoute("ground_tile") + " : [],\n"
+	chunk_str += tab(l) + "],\n"
+
+	l -= 1
+
+	chunk_str += tab(l) + qoute("entities") + " : [],\n"
+	chunk_str += tab(l) + qoute("ground_tile") + " : [],\n"
+
+	l -= 1
+
+	chunk_str += tab(l) + "}\n"
 
 	return chunk;
+
+def getHeader(_keys, _values, _type) :
+
+	l = 0
+
+	header_str = tab(l) + qoute("header") + " :\n{\n"
+
+	l += 1
+
+	for i in range(len(_keys)) :
+
+		value_str = qoute(_value[i]) if _type == "string" else str(value)
+		header_str += tab(l) + qoute(_key[i]) + " : " + value_str
+		header_str += ",\n" if i == len(_keys) - 1 else "\n"
+
+	l -= 1
+
+	header_str += tab(l) + "}\n"
+
+	return header_str
+
+
+
+
+
+
+
+
+
